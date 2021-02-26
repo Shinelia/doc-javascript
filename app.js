a50bfc9528b3bfec5c12d8d9557ff39b17b5a9b1
Shinelia <a.wech62@gmail.com>
2021-02-26 10:19:07 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+/**
+ * Addition function with two numbers
+ * @param {number} a a number
+ * @param {number} b another number
+ * @return {number} returns a+b
+ * 
+ * @exemple
+ * add(1,3) -> 4
+ */
+function add(a,b){
+    return a+b;
+}
+
+add()
